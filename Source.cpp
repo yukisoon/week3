9789ff6b85c1bf74b38e63f659f2e9784d81d788
kisoon <kisoon@gmail.com>
2016-09-20 23:06:01 +0900

    end

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,7 @@
 //http://www.glfw.org/documentation.html
 
 #include <GLFW/glfw3.h>
+#include <stdio.h>
 
 const int width = 640;
 const int height = 480;
@@ -9,86 +10,76 @@ const int height = 480;
 // color = (red, green, blue)
 float *pixels = new float[width * height * 3]; //=malloc
 
-void drawOnePixel(const int& i, const int& j, const float& red, const float& green, const float& blue)
+void drawOnePixel(const int& x, const int& y, const float& red, const float& green, const float& blue)
 {
-	pixels[(i + width*j) * 3 + 0] = red;
-	pixels[(i + width*j) * 3 + 1] = green;
-	pixels[(i + width*j) * 3 + 2] = blue;
+	pixels[(x + width*y) * 3 + 0] = red;
+	pixels[(x + width*y) * 3 + 1] = green;
+	pixels[(x + width*y) * 3 + 2] = blue;
 }
 
-void drawLine(const int& i0, const int& j0, const int& i1, const int& j1, const float& red, const float& green, const float& blue)
+void drawLine(const int& x_s, const int& y_s, const int& x_e, const int& y_e, const float& red, const float& green, const float& blue)
 {
-	for (int i = i0; i <= i1; i++)
+	if (x_e == x_s)
 	{
-		const int j = (j1 - j0)*(i - i0) / (i1 - i0) + j0;
-
-		drawOnePixel(i, j, red, green, blue);
+		for (int y = y_s; y <= y_e; y++)
+			drawOnePixel(x_s, y, red, green, blue);
+	}
+	else
+	{
+		for (int i = x_s; i <= x_e; i++)
+		{
+			const int j = (y_e - y_s)*(i - x_s) / (x_e - x_s) + y_s;
+			drawOnePixel(i, j, red, green, blue);			
+		}
 	}
 }
 
 void draw()
 {
-	const int i_center = 50, j_center = 50;
-	const int thickness = 50;
 
-	/*white background*/
+	/*배경*/
 	for (int j = 0; j < height; j++)
+	{
 		for (int i = 0; i < width; i++)
-		{
 			drawOnePixel(i, j, 1.0f, 1.0f, 1.0f);
-		}
+	}
 
 	/*사선*/
-	drawLine(50, 350, 100, 400, 0.0f, 0.0f, 1.0f);
-	drawLine(50, 351, 100, 401, 0.0f, 0.0f, 1.0f);
-	drawLine(50, 352, 100, 402, 0.0f, 0.0f, 1.0f);
-	drawLine(50, 353, 100, 403, 0.0f, 0.0f, 1.0f);
-	drawLine(50, 354, 100, 404, 0.0f, 0.0f, 1.0f);
-	drawLine(50, 355, 100, 405, 0.0f, 0.0f, 1.0f);
-	drawLine(50, 356, 100, 406, 0.0f, 0.0f, 1.0f);
-	drawLine(50, 357, 100, 407, 0.0f, 0.0f, 1.0f);
-	drawLine(50, 358, 100, 408, 0.0f, 0.0f, 1.0f);
-	drawLine(50, 359, 100, 409, 0.0f, 0.0f, 1.0f);
-	drawLine(50, 360, 100, 410, 0.0f, 0.0f, 1.0f);
+	for (int i = 50; i >= 46; i--)
+	{	
+		static int y = 350;
+
+		for (int j = 0; j < 3; j++)
+		{
+			drawLine(i, y + j, i + 50, (y + j) + 50, 0.0f, 0.0f, 1.0f);
+		}
+
+		y+=2;
+		if (y >= 360)
+			y = 350;
+	}
+
 
 	/*빈사각형*/
-	for (int j = 399; j < 400; j++)//세로
-		for (int i = 200; i < 250; i++)//가로
-		{
-			drawOnePixel(i, j, 1.0f, 0.0f, 0.0f);
-		}
-
-	for (int j = 349; j < 350; j++)//세로
-		for (int i = 200; i < 250; i++)//가로
-		{
-			drawOnePixel(i, j, 1.0f, 0.0f, 0.0f);
-		}
-
-	for (int j = 350; j < 400; j++)//세로
-		for (int i = 249; i < 250; i++)//가로
-		{
-			drawOnePixel(i, j, 1.0f, 0.0f, 0.0f);
-		}
-
-	for (int j = 350; j < 400; j++)//세로
-		for (int i = 199; i < 200; i++)//가로
-		{
-			drawOnePixel(i, j, 1.0f, 0.0f, 0.0f);
-		}
+	drawLine(200, 400, 250, 400, 0.0f, 0.0f, 1.0f);
+	drawLine(200, 350, 250, 350, 0.0f, 0.0f, 1.0f);
+	drawLine(200, 350, 200, 400, 0.0f, 0.0f, 1.0f);
+	drawLine(250, 350, 250, 400, 0.0f, 0.0f, 1.0f);
 
 
 	/*채운사각형*/
-	for (int j = 350; j < 400; j++)//세로
-		for (int i = 300; i < 350; i++)//가로
-		{
-			drawOnePixel(i, j, 1.0f, 0.0f, 0.0f);
-		}
+	for (int j = 350; j < 400; j++)
+	{
+		for (int i = 300; i < 350; i++)
+			drawOnePixel(i, j, 0.0f, 0.0f, 1.0f);
+	}
 
 
 	/*삼각형*/
 	drawLine(50, 50, 100, 100, 0.0f, 0.0f, 1.0f);
 	drawLine(100, 100, 150, 50, 0.0f, 0.0f, 1.0f);
 	drawLine(50, 50, 150, 50, 0.0f, 0.0f, 1.0f);
+
 
 	/*오각형*/
 	drawLine(170, 50, 200, 80, 0.0f, 0.0f, 1.0f);
@@ -97,15 +88,63 @@ void draw()
 	drawLine(220, 30, 230, 50, 0.0f, 0.0f, 1.0f);
 	drawLine(180, 30, 220, 30, 0.0f, 0.0f, 1.0f);
 
+
 	/*원*/
-	for (int i = 50; i < 100; i++)
-		for (int j = 200; j < 250; j = j + 2)
+	/*int u = 25;
+	for (int i = 0; i<u * 2; i++) 
+	{
+		for (int j = 0; j<u * 2; j++) 
 		{
-			drawOnePixel(i, j, 1.0f, 0.0f, 0.0f);
+			if (u*u > (i + 0.5 - u)*(i + 0.5 - u) + (j + 0.5 - u)*(j + 0.5 - u))
+				drawOnePixel(i + 300, j + 10, 0.0f, 0.0f, 1.0f);
 		}
+	}
 
-	//drawLine(100, 100, 150, 50, 0.0f, 0.0f, 1.0f);
+	u = 24;
+	for (int i = 0; i<u * 2; i++)
+	{
+		for (int j = 0; j<u * 2; j++) 
+		{
+			if (u*u > (i + 0.5 - u)*(i + 0.5 - u) + (j + 0.5 - u)*(j + 0.5 - u))
+				drawOnePixel(i + 301, j + 11, 1.0f, 1.0f, 1.0f);
+		}
+	}*/
 
+	drawLine(321, 80, 325, 80, 0.0f, 0.0f, 1.0f); drawLine(326, 80, 330, 80, 0.0f, 0.0f, 1.0f);
+	drawLine(317, 79, 320, 79, 0.0f, 0.0f, 1.0f); drawLine(331, 79, 334, 79, 0.0f, 0.0f, 1.0f);
+	drawLine(315, 78, 317, 78, 0.0f, 0.0f, 1.0f); drawLine(334, 78, 336, 78, 0.0f, 0.0f, 1.0f);
+	drawLine(313, 77, 314, 77, 0.0f, 0.0f, 1.0f); drawLine(337, 77, 338, 77, 0.0f, 0.0f, 1.0f);
+	drawLine(312, 76, 313, 76, 0.0f, 0.0f, 1.0f); drawLine(338, 76, 339, 76, 0.0f, 0.0f, 1.0f);
+	drawLine(310, 75, 311, 75, 0.0f, 0.0f, 1.0f); drawLine(340, 75, 341, 75, 0.0f, 0.0f, 1.0f);
+	drawLine(309, 74, 310, 74, 0.0f, 0.0f, 1.0f); drawLine(341, 74, 342, 74, 0.0f, 0.0f, 1.0f);
+	drawLine(308, 73, 309, 73, 0.0f, 0.0f, 1.0f); drawLine(342, 73, 343, 73, 0.0f, 0.0f, 1.0f);
+	drawLine(307, 72, 308, 72, 0.0f, 0.0f, 1.0f); drawLine(343, 72, 344, 72, 0.0f, 0.0f, 1.0f);
+	drawLine(306, 71, 307, 71, 0.0f, 0.0f, 1.0f); drawLine(344, 71, 345, 71, 0.0f, 0.0f, 1.0f);
+	drawLine(306, 70, 306, 70, 0.0f, 0.0f, 1.0f); drawLine(345, 70, 345, 70, 0.0f, 0.0f, 1.0f);
+	drawLine(305, 69, 305, 69, 0.0f, 0.0f, 1.0f); drawLine(346, 69, 346, 69, 0.0f, 0.0f, 1.0f);
+	drawLine(304, 68, 305, 68, 0.0f, 0.0f, 1.0f); drawLine(346, 68, 347, 68, 0.0f, 0.0f, 1.0f);
+	drawLine(304, 67, 304, 67, 0.0f, 0.0f, 1.0f); drawLine(347, 67, 347, 67, 0.0f, 0.0f, 1.0f);
+	drawLine(303, 64, 303, 66, 0.0f, 0.0f, 1.0f); drawLine(348, 64, 348, 66, 0.0f, 0.0f, 1.0f);
+	drawLine(302, 61, 302, 64, 0.0f, 0.0f, 1.0f); drawLine(349, 61, 349, 64, 0.0f, 0.0f, 1.0f);
+	drawLine(301, 56, 301, 60, 0.0f, 0.0f, 1.0f); drawLine(350, 56, 350, 60, 0.0f, 0.0f, 1.0f);
+	
+	drawLine(301, 51, 301, 55, 0.0f, 0.0f, 1.0f); drawLine(350, 51, 350, 55, 0.0f, 0.0f, 1.0f);
+	drawLine(302, 47, 302, 50, 0.0f, 0.0f, 1.0f); drawLine(349, 47, 349, 50, 0.0f, 0.0f, 1.0f);
+	drawLine(303, 45, 303, 47, 0.0f, 0.0f, 1.0f); drawLine(348, 45, 348, 47, 0.0f, 0.0f, 1.0f);
+	drawLine(304, 44, 304, 44, 0.0f, 0.0f, 1.0f); drawLine(347, 44, 347, 44, 0.0f, 0.0f, 1.0f);
+	drawLine(304, 43, 305, 43, 0.0f, 0.0f, 1.0f); drawLine(346, 43, 347, 43, 0.0f, 0.0f, 1.0f);
+	drawLine(305, 42, 305, 42, 0.0f, 0.0f, 1.0f); drawLine(346, 42, 346, 42, 0.0f, 0.0f, 1.0f);
+	drawLine(306, 41, 306, 41, 0.0f, 0.0f, 1.0f); drawLine(345, 41, 345, 41, 0.0f, 0.0f, 1.0f);
+	drawLine(306, 40, 307, 40, 0.0f, 0.0f, 1.0f); drawLine(344, 40, 345, 40, 0.0f, 0.0f, 1.0f);
+	drawLine(307, 39, 308, 39, 0.0f, 0.0f, 1.0f); drawLine(343, 39, 344, 39, 0.0f, 0.0f, 1.0f);
+	drawLine(308, 38, 309, 38, 0.0f, 0.0f, 1.0f); drawLine(342, 38, 343, 38, 0.0f, 0.0f, 1.0f);
+	drawLine(309, 37, 310, 37, 0.0f, 0.0f, 1.0f); drawLine(341, 37, 342, 37, 0.0f, 0.0f, 1.0f);
+	drawLine(310, 36, 311, 36, 0.0f, 0.0f, 1.0f); drawLine(340, 36, 341, 36, 0.0f, 0.0f, 1.0f);
+	drawLine(312, 35, 313, 35, 0.0f, 0.0f, 1.0f); drawLine(338, 35, 339, 35, 0.0f, 0.0f, 1.0f);
+	drawLine(313, 34, 314, 34, 0.0f, 0.0f, 1.0f); drawLine(337, 34, 338, 34, 0.0f, 0.0f, 1.0f);
+	drawLine(315, 33, 317, 33, 0.0f, 0.0f, 1.0f); drawLine(334, 33, 336, 33, 0.0f, 0.0f, 1.0f);
+	drawLine(317, 32, 320, 32, 0.0f, 0.0f, 1.0f); drawLine(331, 32, 334, 32, 0.0f, 0.0f, 1.0f);
+	drawLine(321, 31, 325, 31, 0.0f, 0.0f, 1.0f); drawLine(326, 31, 330, 31, 0.0f, 0.0f, 1.0f);
 
 }
 
